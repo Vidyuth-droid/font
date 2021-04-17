69effbf25864a1f07526619f23178df03e26c762
Vidyuth-droid <72655577+Vidyuth-droid@users.noreply.github.com>
2021-04-17 17:54:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+leftwrist=0;
+rightwrist=0;
+difference=0;
 function setup(){
 video=createCapture(VIDEO);
 video.size(550,500)
@@ -8,7 +11,9 @@ poseNet.on("pose",gotPoses);
 }
 function draw(){
 background("#fed8b1");
-
+textSize(difference);
+fill("#ffd9f9");
+text("Name", 50,400);
 }
 function modelLoaded(){
     console.log("Model is loaded");
@@ -16,5 +21,10 @@ function modelLoaded(){
 function gotPoses(results){
 if (results.length>0){
     console.log(results);
+    leftwrist=results[0].pose.leftWrist.x;
+    rightwrist=results[0].pose.rightWrist.x;
+difference=leftwrist-rightwrist;
+console.log("The difference between left wrist and right wrist is "+difference);
+console.log("The left wrist is "+leftwrist+" The right wrist is "+rightwrist);
 }
 }
